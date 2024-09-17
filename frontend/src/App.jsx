4f9b80fce00265f96f5c6e069923848777f746d0
Amal Raj <amalrajkvdev@gmail.com>
2024-09-17 21:33:21 +0530

    edit root routes app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -119,7 +119,8 @@ function App() {
             </RedirectAuthenticatedUser>
           }
         />
-        
+        {/* catch all routes */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <Toaster />
     </div>
