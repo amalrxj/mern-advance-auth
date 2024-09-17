6b541d4e860c86dcc9e0d64f7cc938b39c190433
Amal Raj <amalrajkvdev@gmail.com>
2024-09-17 21:16:35 +0530

    new eero fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -119,8 +119,7 @@ function App() {
             </RedirectAuthenticatedUser>
           }
         />
-        {/* catch all routes */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        
       </Routes>
       <Toaster />
     </div>
